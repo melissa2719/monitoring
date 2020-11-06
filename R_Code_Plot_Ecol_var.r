0956e68f269937ec44ad3198dfc3499bd0f8fe44
melissa2719 <74042217+melissa2719@users.noreply.github.com>
2020-11-06 11:08:08 +0100

    Create R_Code_Plot_Ecol_var.r

---
+++ R_Code_Plot_Ecol_var.r
@@ -0,0 +1,34 @@
+#6 nov
+
+# R code for plotting the relationship among ecological variables
+
+install.packages("sp")
+
+
+data(meuse)
+
+
+
+View(meuse)
+head(meuse)
+summary(meuse)
+
+#exercise: plot zinc(y) against cadmium(x)
+#error
+plot(cadmium,zinc)
+error in evaluating the argument 'x' 
+
+# $
+plot(meuse$cadmium, meuse$zinc)
+
+#if i use attach i don't need to use $
+attach(meuse)
+plot(cadmium, zinc)
+
+plot(cadmium, lead)
+
+#scatterplot Matrices
+pairs(meuse)
+
+#pairing only elements part of dataset: how to do that?
+#only with cadmium, copper, lead, zinc...
