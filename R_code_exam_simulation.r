5bfd76ea33a057afeeff700a679a2d74184d1a3a
melissamaini <74042217+melissa2719@users.noreply.github.com>
2021-01-11 10:47:27 +0100

    Update R_code_exam_simulation.r

--- R_code_exam_simulation.r
+++ R_code_exam_simulation.r
@@ -4,4 +4,27 @@
 install.packages("ncdf4")
 library(ncdf4)
 
-setwd(":C/lab")
+setwd("C:/lab/")
+
+library(raster)
+tjan <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc")
+plot(tjan)
+
+#color ramp palette
+cltjan <- colorRampPalette(c('darkslategrey','darkslategray4','darkslategray1'))(100)
+plot(tjan, col=cltjan)
+
+cltjan <- colorRampPalette(c('blue','yellow','red'))(100) 
+plot(tjan, col=cltjan)
+
+#same for october data
+toct <- raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc")
+plot(toct, col=cltjan)
+
+#compare them difference between Temperature
+dift <- tjan - toct
+cldif <- colorRampPalette(c('blue','white','red'))(100) #
+plot(dift, col=cldif)
+
+
+
